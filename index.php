6ec2a94590ea08abf2b4c2deb694a54ab2c82f17
George Mapili <georgemapili322@gmail.com>
2021-03-11 01:16:57 +0800

    try update index for remote mysql part2

--- index.php
+++ index.php
@@ -85,7 +85,58 @@ if (isset($_POST['login'])) {
 ?>
 
 <body>
-    <h1>Hello</h1>
+    <h2 class="display-4 mb-3" style="color: rgb(15, 208, 214);">Welcome to SUMC Doctors Clinic</h2>
+    <?= (isset($_GET['RegSuccess'])) ? '<span class="text-success my-4">Register Successfully</span>' : ""; ?>
+    <form action="index.php" method="post">
+
+        <div class="form-group">
+            <label>Email address</label>
+            <?= (isset($_GET['errEmail'])) ? '<input type="email" name="email" class="form-control is-invalid" required>' : '<input type="email" name="email" class="form-control" required>'; ?>
+            <?= (isset($_GET['errEmail'])) ? '<span class="text-danger">Incorrect Email</span>' : ''; ?>
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <?= (isset($_GET['errPass'])) ? '<input type="password" name="password" class="form-control is-invalid" required>' : '<input type="password" name="password" class="form-control" required>'; ?>
+            <?= (isset($_GET['errPass'])) ? '<span class="text-danger">Incorrect Password</span>' : ''; ?><br>
+            <?php
+
+            if (isset($_GET['pid'])) {
+                $time = time() - 30;
+                $sql = "SELECT COUNT(*) AS total_count FROM loginlog WHERE patient_id = :id AND try_time > :time";
+                $stmt = $con->prepare($sql);
+                $stmt->bindParam(":id", $_GET['pid'], PDO::PARAM_INT);
+                $stmt->bindParam(":time", $time, PDO::PARAM_INT);
+                $stmt->execute();
+
+                $log = $stmt->fetch(PDO::FETCH_ASSOC);
+                $logCount = $log['total_count'];
+            }
+            ?>
+            <?= (isset($_GET['pid']) && (3 - $logCount) >= 0) ? '<span class="text-danger"> tries left:  ' . $totalCount = (3 - $logCount) . '' : ''; ?><br>
+        </div>
+        <?php
+        if (isset($_GET['pid']) && (int) (3 - $logCount) <= 0) { ?>
+            <div class="text-center">
+                <a href="#" class="btn-block btn-info mt-4 disabled">Login</a>
+                <p class="text-danger mt-3">Wait for 30 seconds</p>
+            </div>
+        <?php
+        } else {
+        ?>
+            <input type="submit" class="btn-block btn-info mt-4" value="Login" name="login">
+        <?php
+        }
+        ?>
+
+        <div class="text-center mt-5">
+            <a class="btn btn-danger" href="register.php">No account yet?</a>
+        </div>
+    </form>
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
