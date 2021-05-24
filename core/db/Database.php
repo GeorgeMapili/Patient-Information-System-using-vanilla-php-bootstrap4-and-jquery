2bc3a64c9d5189a58a03d6df9e433131cb096486
George Mapili <georgemapili322@gmail.com>
2021-05-24 15:59:33 +0800

    test dep

--- core/db/Database.php
+++ core/db/Database.php
@@ -9,10 +9,17 @@ class Database
     public function connect(){
         date_default_timezone_set('Asia/Manila');
 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "pis";
+        // Development Connection
+        // $servername = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $dbname = "pis";
+
+        // Remote DB Connection
+        $servername = "remotemysql.com";
+        $username = "QA9u2YYTw5";
+        $password = "zoTCUABrxM";
+        $dbname = "QA9u2YYTw5";
 
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
