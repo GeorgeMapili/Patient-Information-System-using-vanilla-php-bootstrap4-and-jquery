0a4c7dee21d29da472c34cfc7e4cccc8d5346d98
George Mapili <georgemapili322@gmail.com>
2021-07-10 14:43:15 +0800

    dep. 2

--- core/patient/Register.php
+++ core/patient/Register.php
@@ -5,6 +5,10 @@ require_once(dirname(dirname(__DIR__)). "/vendor/autoload.php");
 use PDO;
 use core\db\Database;
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 class register extends Database
 {
 
