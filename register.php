b464eb4f69ce20ad0400e56534efa8940a0d194b
George Mapili <georgemapili322@gmail.com>
2021-07-10 14:40:43 +0800

    dep. 1

--- register.php
+++ register.php
@@ -6,6 +6,11 @@ if (isset($_SESSION['id'])) {
     header("location:home.php");
     exit(0);
 }
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 ?>
 <!doctype html>
 <html lang="en">
